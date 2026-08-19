ba4e0342de915bfa927069b40461d6e355416d04
bhimagar <bm477601@gmail.com>
2026-08-19 16:01:36 +0545

    Day 1

---
+++ day1.py
@@ -0,0 +1,20 @@
+# ctrl + ` - to open terminal
+# ctrl + / - enable/disable comment or "shift + 3"
+# comments: starts with # lines written after # are ignoted, mostly used for describing codes
+
+
+# variables: container used to store data temporarily
+a1 a2 = "hello hello hello hello hello hello"
+print(a1 a2)
+
+# Case
+# Snake case
+my_variable = "hello world"
+
+# Camel case
+myVariableFirstSecond = "hello world"
+
+# Pascal case
+MyVariableFirstSecond = "hello world"
+
+# Datatype: 
