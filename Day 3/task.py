671398038b0325ee0457565cd490d2bf2ca11da5
bhimagar <bm477601@gmail.com>
2026-08-23 21:04:07 +0545

    Day 3 task

--- Day 3/task.py
+++ Day 3/task.py
@@ -1,4 +1,5 @@
 # task: 
+
 # define a number, check wether the number is divisible by both 3 and 5
 a = 15
 
@@ -9,11 +10,23 @@ username = ['Ram', 'Sita', 'Hari']
 
 print('Ram' in username)
 
+# check if a exists in b
 a = [1, 2, 3]
 b = [1, 2, 3]
-# check if a exists in b
 
+print(a == b)
 
 # define 2 numbers and create a simple calculator(+, -, *, /)
+a = 1
+b = 2
+
+print("Add:", a + b)
+print("Minus:", a - b)
+print("Multiply:", a * b)
+print("Divide:", a / b)
 
 # define age=16 and id=True, check whether the age is greater than 18 and id is True
+age = 16
+id = True
+
+print(age > 18 and id == True)
