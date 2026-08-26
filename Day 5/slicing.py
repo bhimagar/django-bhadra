85eab8be3c9da96e5e855402e6c5303f723e1b75
bhimagar <bm477601@gmail.com>
2026-08-26 13:16:28 +0545

    Day 5

---
+++ Day 5/slicing.py
@@ -0,0 +1,7 @@
+# name = "Harry, Shubham"
+# print(len(name))
+# print(name[0:5])
+fruit = "Mango"
+mangoLen = len(fruit)
+print(fruit[0:4])
+# print("Mango is a", len1, "letter word")
---
+++ Day 5/task2.py
@@ -0,0 +1,41 @@
+# todo:
+
+text = "Python Programming"
+# print out first character
+print(text[0:1])
+# print out las character
+print(text[17:18])
+# print the 3 index character
+print(text[3])
+# print out Python
+print(text[0:7])
+# print out Programming
+print(text[7:])
+# print out Program
+print(text[7:14])
+# print out first 5 character (also negative index)
+print(text[:5])
+print(text[:-13])
+# print out last 5 characters (also negative index)
+print(text[13:])
+print(text[-5:])
+# print out the character of even index
+print(text[::2])
+# print out the character of odd index
+print(text[::3])
+# print the reverse of the text (output: ginmmargorP nohtyP)
+print(text[::-1])
+
+numbers = [12,52,63,87,5,6]
+# print out 1st data
+print(numbers[0])
+# print out last data
+print(numbers[5])
+# print the 5 index character
+print(numbers[5])
+# print out first 3 character
+print(numbers[:3])
+# print out last 3 characters
+print(numbers[3:])
+# get [52,63,87]
+print(numbers[1:4])
