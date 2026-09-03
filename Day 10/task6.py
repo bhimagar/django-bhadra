925800534e63a33250d2e1965e48075cbcc80fa1
bhimagar <bm477601@gmail.com>
2026-09-03 10:46:00 +0545

    Day 10 task

---
+++ Day 10/task6.py
@@ -0,0 +1,55 @@
+# todo
+
+# create a function add 2 parameter
+# the function should add 2 parameter and provide the result
+# get 2 numbers from user and use it as arguments during function call
+
+# num1 = int(input("Num1:"))
+# num2 = int(input("Num2:"))
+
+# def add(num1, num2):
+#     print("Add:", num1 + num2)
+    
+# add(num1, num2)
+
+
+
+# create a function add 2 parameter
+# the function should subtract 2 parameter and provide the result
+# get 2 numbers from user and use it as arguments during function call
+
+# num1 = int(input("Num1:"))
+# num2 = int(input("Num2:"))
+
+# def sub(num1, num2):
+#     print("Subtract:", num1 - num2)
+    
+# sub(num1, num2)
+
+
+
+# create a function add 2 parameter
+# the function should multiply 2 parameter and provide the result
+# get 2 numbers from user and use it as arguments during function call
+
+# num1 = int(input("Num1:"))
+# num2 = int(input("Num2:"))
+
+# def mul(num1, num2):
+#     print("Multiply:", num1 * num2)
+    
+# mul(num1, num2)
+
+
+
+# create a function add 2 parameter
+# the function should divide 2 parameter and provide the result
+# get 2 numbers from user and use it as arguments during function call
+
+num1 = int(input("Num1:"))
+num2 = int(input("Num2:"))
+
+def div(num1, num2):
+    print("Divide:", num1 / num2)
+    
+div(num1, num2)
